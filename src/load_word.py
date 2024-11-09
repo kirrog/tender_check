389eb0d6d587b701e8e5e81a2f378555a92c3e95
kirrog <rroogg76aacchh@gmail.com>
2024-11-09 18:37:17 +0300

    [feature]: entities and comments about logic

--- src/load_word.py
+++ src/load_word.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import docx
 
 
-def read_pdf2text(pdf_file_path: Path) -> str:
+def read_word2text(pdf_file_path: Path) -> str:
     doc = docx.Document(str(pdf_file_path))
     texts = []
     for paragraph in doc.paragraphs:
@@ -13,5 +13,5 @@ def read_pdf2text(pdf_file_path: Path) -> str:
 
 if __name__ == "__main__":
     file_path = Path('/home/kirrog/projects/hackatons/tender_check/cases/9864533/ТЗ № П-ПУиЗР-2024.docx')
-    text = read_pdf2text(file_path)
+    text = read_word2text(file_path)
     print(text)
