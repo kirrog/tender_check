44a2e299eb5d9f5843ff7f6a9112fcc025a2c0c6
ZltZZZ <dudalevadudaleva@mail.ru>
2024-11-09 21:22:15 +0300

    parser.py: +download files, +get full specification

--- src/parser.py
+++ src/parser.py
@@ -8,4 +8,20 @@ parser.add_argument('-url','--url', help='URL for KC', required=True)
 args = parser.parse_args()
 
 resp = requests.get(f'https://zakupki.mos.ru/newapi/api/Auction/Get?auctionId={args.url.rsplit("/", 1)[-1]}')
-pprint(resp.json())
+resp_json = resp.json()
+
+for item in resp_json['items']:
+    item_resp = requests.get(f'https://zakupki.mos.ru/newapi/api/Auction/GetAuctionItemAdditionalInfo?itemId={item["id"]}')
+    item['specification'] = item_resp.json()
+
+
+for file in resp_json['files']:
+    url = f'https://zakupki.mos.ru/newapi/api/FileStorage/Download?id={file["id"]}'
+    file['data'] = b''
+
+    r = requests.get(url)
+    for chunk in r.iter_content(chunk_size=512 * 1024):
+        if chunk:
+            file['data'] += chunk
+
+pprint(resp_json)
