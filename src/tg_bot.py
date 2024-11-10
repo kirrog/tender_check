ce59db4d173d9ff5917df2e7747f4a734c7c277c
ZltZZZ <dudalevadudaleva@mail.ru>
2024-11-10 14:28:43 +0300

    Update tg_bot.py

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -1,16 +1,13 @@
 import json
 
 import telebot
-from telebot import types
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-from src.main import check_case
-from src.parser import parse_data_from_url
+from main import check_case
+from parser import parse_data_from_url
 
-token = None
+token = "7725548623:AAEEI6tTTaFWvolQ2bv2yvK91W3BBr6xEi4"
 bot = telebot.TeleBot(token=token)
 
-user_state = {}
 
 def process_url(message):
     url2process = message.text
@@ -20,34 +17,115 @@ def process_url(message):
     answers = check_case(resp_json)
     print(answers)
 
-    for k, v in answers.items():
-        bot.send_message(message.chat.id, f"{k}: {v}")
+    if answers['project']['isContractGuaranteeRequired']['value_'] == 'true':
+        obespechenie_str = f"""Требуется
+        Размер обеспечения на сайте: {answers['project']['contractGuaranteeAmount']['value_']}
+        Совпадение в проекте: {answers['project']['contractGuaranteeAmount']['leven_partial_ratio']}%
+        Совпадение в тз: {answers['technical']['contractGuaranteeAmount']['leven_partial_ratio']}%
+        """
+    else:
+        obespechenie_str = """Не требуется
+        """
 
-    markup = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    button1 = KeyboardButton('Снять с публикации КС')
-    button2 = KeyboardButton('Подтвердить корректность КС')
+    if 'licenseFiles_0' in answers['project']:
+        sertificates_str = f"""Присутвует
+        Наименование на сайте: {answers['project']['licenseFiles_0']['value_']['name']}
+        Совпадение в проекте: {answers['project']['licenseFiles_0']['leven_partial_ratio']}%
+        Совпадение в тз: {answers['technical']['licenseFiles_0']['leven_partial_ratio']}%
+        """
+    else:
+        sertificates_str = """Отсутствует
+        """
 
-    markup.add(button1, button2)
-    bot.send_message(message.chat.id, "Выберите одну из опций:", reply_markup=markup)
+
+    if 'startCost' in answers['project']:
+        cost_needed_str = f"""Начальная цена: присутствует на сайте
+        Совпадение в проекте: {answers['project']['startCost']['leven_partial_ratio']}%
+        Совпадение в тз: {answers['technical']['startCost']['leven_partial_ratio']}%
+
+        Максимальное значение цены контракта: отсутствет на сайте
+        """
+    else:
+        cost_needed_str = f"""Начальная цена: отсутствует на сайте
+
+                Максимальное значение цены контракта: присутствует на сайте
+                Совпадение в проекте: {answers['project']['startCost']['leven_partial_ratio']}%
+                Совпадение в тз: {answers['technical']['startCost']['leven_partial_ratio']}%
+               """
+
+    date = ""
+    i = 0
+    for i in range(99999):
+        if f'deliveries_{str(i)}_periodDateFrom' in answers['project'] and answers['project'][f'deliveries_{str(i)}_periodDateFrom']['value_'] != 'None':
+            date += f"Этап {str(i)}\n"
+            date += f"""Дата начала поставки на сайте: {answers['project'][f'deliveries_{str(i)}_periodDateFrom']['value_']}
+            Совпадение в проекте: {answers['project'][f'deliveries_{str(i)}_periodDateFrom']['leven_partial_ratio']}%
+            Совпадение в тз: {answers['technical'][f'deliveries_{str(i)}_periodDateFrom']['leven_partial_ratio']}%\n
+            """
+        else:
+            date += "\n"
+            break
+
+        if f'deliveries_{str(i)}_periodDateTo' in answers['project']:
+            date += f"""Дата начала поставки на сайте: {answers['project'][f'deliveries_{str(i)}_periodDateTo']['value_']}
+            Совпадение в проекте: {answers['project'][f'deliveries_{str(i)}_periodDateTo']['leven_partial_ratio']}%
+            Совпадение в тз: {answers['technical'][f'deliveries_{str(i)}_periodDateTo']['leven_partial_ratio']}%\n
+            """
+
+        else:
+            date += "\n"
+            break
 
 
-# Обработка сообщений
-@bot.message_handler(func=lambda message: message.text == "Снять с публикации КС")
-def remove_from_publication(message):
-    user_state[message.chat.id] = 'waiting_for_reason'
-    bot.send_message(message.chat.id, "Пожалуйста, укажите причину снятия с публикации",
-                     reply_markup=types.ReplyKeyboardRemove())
+    i = 0
+    if 'items_0_name' in answers['technical']:
+        specification_str = "ТЗ присутствует\n"
+    else:
+        specification_str = "ТЗ отсутствует"
+    for i in range(99999):
+        if f'items_{str(i)}_name' in answers['technical']:
+            specification_str += f"""  
+            Наименование спецификации на сайте: {answers['technical'][f'items_{str(i)}_name']['value_']}
+            Совпадение в ТЗ: {answers['technical'][f'items_{str(i)}_name']['leven_partial_ratio']}%
+            Количество предметов на сайте: {answers['technical'][f'items_{str(i)}_currentValue']['value_']}
+            Совпадение в ТЗ: {answers['technical'][f'items_{str(i)}_currentValue']['leven_partial_ratio']}%
+            """
+        else:
+            break
 
-# Обработка текста с причиной
-@bot.message_handler(func=lambda message: user_state.get(message.chat.id) == 'waiting_for_reason')
-def handle_reason(message):
-    reason = message.text
-    bot.send_message(message.chat.id, f"Снято с публикации по причине: {reason}")
-    user_state[message.chat.id] = None
 
-@bot.message_handler(func=lambda message: message.text == "Подтвердить корректность КС")
-def confirm_correctness(message):
-    bot.send_message(message.chat.id, "КС подтверждена!", reply_markup=types.ReplyKeyboardRemove())
+    answer = f"""
+    1. Название КС
+    На сайте: {answers['project']['name']['value_']}
+    Совпадение в проекте: {int(answers['project']['name']['leven_partial_ratio'])}%
+    Совпадение в тз: {int(answers['technical']['name']['leven_partial_ratio'])}%
+    
+    2. Обеспечение исполнения контракта
+    {obespechenie_str}
+    
+    3. Наличие сертификатов/лицензий
+    {sertificates_str}
+    
+    4. График поставки
+    {date}
+  
+    5. Цена контракта
+    {cost_needed_str}
+ 
+    6. Спецификация
+    {specification_str}
+    """
+
+    # answers['project']['name'] answers['technical']['name'] # Вывести имя (1)
+    # answers['project']['isContractGuarateeRequired']['value'] answers['technical']['isContractGuarateeRequired']['value']# если да, то напечатать: answers['ContractGuaranteeAmount'] (2)
+    # answers['project']['startCost']['value'] # если есть, то вывести (5)
+    # answers['technical']["items_0_name"] # имя спецификации (6)
+    # answers['technical']["items_0_currentValue"]  # количество в спецификации (6)
+
+    # for k, v in answers.items():
+    #     bot.send_message(message.chat.id, f"{k}: {v}")
+
+    bot.send_message(message.chat.id, answer)
 
 
 @bot.message_handler(commands=['start'])
